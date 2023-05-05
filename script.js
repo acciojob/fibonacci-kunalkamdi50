a4fc07c8882a52044fc4792c8461c337bec71060
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-05 16:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let prev = 0;
+    let curr = 1;
+    for (let i = 2; i <= num; i++) {
+      let next = prev + curr;
+      prev = curr;
+      curr = next;
+    }
+    return curr;
+  }
 }
 
 module.exports = fibonacci;
